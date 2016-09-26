09c229c0c7cac26a7b601f6b40fb939b044ece06
Makoto Shimazu <shimazu@chromium.org>
2016-09-26 09:58:41 +0900

    Remove a comment

--- nocache_server.py
+++ nocache_server.py
@@ -8,7 +8,6 @@ class NoCacheHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    #SimpleHTTPServer.test(HandlerClass=NoCacheHTTPRequestHandler)
     httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 8091), NoCacheHTTPRequestHandler)
     sa = httpd.socket.getsockname()
     print "Serving HTTP on %s port %d" % (sa[0], sa[1])
