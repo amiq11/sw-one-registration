6ae5b09b14a96057c5a7c18bd8ada9e8b8bec48d
Makoto Shimazu <shimazu@chromium.org>
2016-09-26 09:58:16 +0900

    Listen only to the local address

--- nocache_server.py
+++ nocache_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import SimpleHTTPServer
+import BaseHTTPServer
 
 class NoCacheHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -7,4 +8,8 @@ class NoCacheHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    SimpleHTTPServer.test(HandlerClass=NoCacheHTTPRequestHandler)
+    #SimpleHTTPServer.test(HandlerClass=NoCacheHTTPRequestHandler)
+    httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 8091), NoCacheHTTPRequestHandler)
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP on %s port %d" % (sa[0], sa[1])
+    httpd.serve_forever()
