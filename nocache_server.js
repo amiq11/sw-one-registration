c6de9584757f65817be6177ffb2ae920c9bf459b
Makoto Shimazu <makoto.shimaz@gmail.com>
2018-01-12 17:27:15 +0900

    Add heavy.html for slow response and variable script for update process

--- nocache_server.js
+++ nocache_server.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const stream = require('express-stream');
+const fs = require('fs');
+const SlowStream = require('slow-stream');
+const ejs  = require('ejs');
+
 const app = express();
 
 console.log('starting server...');
-
 let server = app.listen(8080, '127.0.0.1', () => {
   console.log(`listening: ${server.address().address}:${server.address().port}`);
 });
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
 
 app.use(express.static(`${__dirname}/public`,
                        {setHeaders: (res, path, stat) => {
                          res.set('Cache-Control', 'no-store');
                        }}));
 
-app.get('/sw-blank-variant.js', (req, res) => {
-  res.set('Cache-Control', 'no-store');
+app.get('/:name\.js', (req, res) => {
   res.type('text/javascript');
-  res.send(`// ${Date.now()}`);
+  console.log(req.params.name);
+  res.render(req.params.name, {date: Date.now()});
+});
+
+app.get('/slow/:name', stream.pipe(), (req, res) => {
+  let filepath = `${__dirname}/public/${req.params.name}`;
+  fs.createReadStream(filepath, {bufferSize: 64})
+      .pipe(new SlowStream({maxWriteInterval: 100}))
+      .pipe(res);
 });
--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,32 @@
-importScripts('loong-imported-script.js');
+// importScripts('loong-imported-script.js');
 
-// Check if a warning shows up
-setTimeout(() => {
-    addEventListener('install', e => {
-        console.log('installing...');
-      });
-  }, 100);
+// // Check if a warning shows up
+// setTimeout(() => {
+//     addEventListener('install', e => {
+//         console.log('installing...');
+//       });
+//   }, 100);
 
 addEventListener('install', e => {
   console.log('installing...');
-  // e.waitUntil(Promise.reject());
+  console.log(`getRegistration: ${registration.scope}`);
 });
 
 addEventListener('activate', e => {
   console.log('activating...');
-  e.waitUntil(Promise.reject());
 });
 
 // Adds fetch handler not to skip this script
 addEventListener('fetch', e => {
-    if (e.request.url.indexOf('text') > 0) {
-      e.respondWith(fetch('foo.txt'));
-    } else {
-      e.respondWith(new Response('intercepted'));
-    }
-  });
+  if (e.request.url.indexOf('text') > 0) {
+    e.respondWith(fetch('foo.txt'));
+    return;
+  } else if (e.request.url.indexOf('intercepted') > 0){
+    e.respondWith(new Response('intercepted'));
+    return;
+  }
+  e.respondWith(fetch(e.request));
+});
 
-importScripts('small-imported-script.js', 'small-imported-script.js');
+// // Import a script twice.
+// importScripts('small-imported-script.js', 'small-imported-script.js');
